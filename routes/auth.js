508fc9124349c44d59f4957369a83b5075c0cdf3
Daniel Rojas Alday <102681651+danielestebanRA@users.noreply.github.com>
2025-11-17 20:03:41 -0300

    eliminacion de views

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const userModel = require(path.join(__dirname, '..', 'lib', 'userModel'));
-
 const ensureAuth = require(path.join(__dirname, '..', 'middleware', 'ensureAuth'));
 
 // Signup form
